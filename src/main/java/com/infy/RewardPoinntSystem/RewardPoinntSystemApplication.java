6afd17da9cec3f09738cdd8b371e3be0c382812b
jivan899 <jivan.mahajan37@gmail.com>
2025-03-20 16:07:14 +0530

    Added Swagger configuration

--- src/main/java/com/infy/RewardPoinntSystem/RewardPoinntSystemApplication.java
+++ src/main/java/com/infy/RewardPoinntSystem/RewardPoinntSystemApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.infy.RewardPoinntSystem.entity")
 @EnableJpaRepositories(basePackages = "com.infy.RewardPoinntSystem.repository")
+@EnableWebMvc
 public class RewardPoinntSystemApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/infy/RewardPoinntSystem/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.infy.RewardPoinntSystem.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("Infy Reward System")
+//                        .version("1.0")
+//                        .description("API documentation for Reward Poin System"));
+//    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Blogging Application")
+                        .version("1.0")
+                        .description("API documentation for Reward Poin System")
+                        .contact(new Contact()
+                                .name("Jivan Mahajan")
+                                .url("https://google.com")
+                                .email("jivan.mahajan37@gmail.com"))
+                        .license(new License()
+                                .name("License of API")
+                                .url("https://google.com")));
+    }
+    
+
+}
+
+
